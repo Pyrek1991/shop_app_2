f1ab0faedde262b3507699c6ded2b52b42888bc2
Piotr <piot.domzalski91@gmail.com>
2023-01-09 19:40:55 +0100

    appearance of the product

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,37 @@
+// PL określa jak ma wyglądać każdy produkt
+// EN defines what each product should look like
+
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(                          // PL ikona po lewej, EN icon left
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(                       // PL ikona po prawej, EN icon right
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
