d561b80012b4e7a966a84af9960d83d2095420fb
Piotr <piot.domzalski91@gmail.com>
2023-01-09 19:11:15 +0100

    display products

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,12 @@
+// PL bedzie prezentować wszystkie produkty które posiadamy
+// EN will display all the products we have
+
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
