6a4695f82b211f37e8740dc02ab14bf0858c962b
Piotr <piot.domzalski91@gmail.com>
2023-02-05 10:30:26 +0100

    add method clean

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -66,6 +66,10 @@ class Cart with ChangeNotifier {
   void removeItem(String productId) {
     _items.remove(productId);
     notifyListeners();
+  }
 
+  void clear() {
+    _items = {};
+    notifyListeners();
   }
 }
