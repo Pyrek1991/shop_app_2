9ee07e9c5c29268b9895e3a34580aba0653688f9
Piotr <piot.domzalski91@gmail.com>
2023-02-14 14:37:16 +0100

    service scaffoldMessenger UNDO

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -22,13 +22,13 @@ class Cart with ChangeNotifier {
   }
 
   int get itemCount {
-  return _items.length;
+    return _items.length;
   }
 
   double get totalAmount {
     double total = 0.0;
     _items.forEach((key, cartItem) {
-      total += cartItem.price + cartItem.quantity;
+      total += cartItem.price * cartItem.quantity;
     });
     return total;
   }
@@ -65,6 +65,25 @@ class Cart with ChangeNotifier {
 
   void removeItem(String productId) {
     _items.remove(productId);
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                price: existingCartItem.price,
+                quantity: existingCartItem.quantity - 1,
+              ));
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 
