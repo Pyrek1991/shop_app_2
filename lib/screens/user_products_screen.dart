14dc450a32ab84a9173c7e95c1fd202b9b8db877
Piotr <piot.domzalski91@gmail.com>
2023-02-15 10:28:07 +0100

    add list and title or image products

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
 
 class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -19,9 +23,22 @@ class UserProductsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(padding: EdgeInsets.all(8),
-        child: ListView.builder(itemCount:productsData.items.length,
-        itemBuilder: (_, i) => ,),),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
