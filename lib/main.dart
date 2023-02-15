9b4d4c60853f84b9a18acfaec03c9c21572bf402
Piotr <piot.domzalski91@gmail.com>
2023-02-15 10:26:40 +0100

    add routeName user

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -38,6 +39,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeNamed: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx)  => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
