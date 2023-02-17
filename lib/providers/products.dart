ec19209fa92e896f89db4a77aef071c39cf1aa3b
Piotr <piot.domzalski91@gmail.com>
2023-02-17 12:51:22 +0100

    update

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -69,24 +69,32 @@ class Products with ChangeNotifier {
   }*/
 
   void addProduct(Product product) {
-    final url = Uri.parse('https://flutter-update-1d5bf-default-rtdb.europe-west1.firebasedatabase.app/products.json');
-    http.post(url, body: json.encode({
-      'title': product.title,
-      'description': product.description,
-      'imageUrl': product.imageUrl,
-      'price': product.price,
-      'isFavorite': product.isFavorite,
-    }),);
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); // at the start of the list
-    notifyListeners();
+    final url = Uri.parse(
+        'https://flutter-update-1d5bf-default-rtdb.europe-west1.firebasedatabase.app/products.json');
+    http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+          //print(json.decode(response.body));
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // at the start of the list
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
@@ -103,5 +111,4 @@ class Products with ChangeNotifier {
     _items.removeWhere((element) => element.id == id);
     notifyListeners();
   }
-
 }
