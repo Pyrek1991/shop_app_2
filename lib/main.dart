c10883e8937afa5acad42770af1f287dce8a19ca
Piotr <piot.domzalski91@gmail.com>
2023-02-18 22:01:57 +0100

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app_2/screens/cart_screen.dart';
 import './providers/cart.dart';
-
 import './screens/products_overview_screen.dart';
+
+import './providers/auth.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/orders.dart';
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
@@ -40,7 +44,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeNamed: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.routeName: (ctx)  => OrdersScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
           EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
