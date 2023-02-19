b23fe3600956d3627ea6205c376721c9f4af4223
Piotr <piot.domzalski91@gmail.com>
2023-02-19 10:03:04 +0100

    login user

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -11,13 +11,28 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get token {
+    if (_expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
+
   static const params = {
-    'key': 'AIzaSyBzUK1SjoCIMypZKE72GAXM5xUR8ahIi1E', // Here you enter your own key
+    'key': 'AIzaSyBzUK1SjoCIMypZKE72GAXM5xUR8ahIi1E',
+    // Here you enter your own key
   };
 
-  Future<void> _authenticate(String email, String password, String urlSegment) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.https(
-        'identitytoolkit.googleapis.com','/v1/accounts:$urlSegment', params);
+        'identitytoolkit.googleapis.com', '/v1/accounts:$urlSegment', params);
     try {
       final response = await http.post(
         url,
@@ -30,9 +45,17 @@ class Auth with ChangeNotifier {
         ),
       );
       final responseData = json.decode(response.body);
-      if(responseData['error'] != null) {
+      if (responseData['error'] != null) {
         throw HttpException(responseData['error']['message']);
       }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(responseData['expiresIn']),
+        ),
+      );
+      notifyListeners();
     } catch (error) {
       throw error;
     }
